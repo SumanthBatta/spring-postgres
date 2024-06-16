669b780e4277fc5d83e481eb423f68917de0dc87
Batta <battasumanth1091@gmail.com>
2024-06-16 19:33:43 +0530

    add crud operation

--- src/main/java/com/postgres/postgre/PersonController.java
+++ src/main/java/com/postgres/postgre/PersonController.java
@@ -4,10 +4,11 @@ package com.postgres.postgre;
 import com.postgres.postgre.model.Person;
 import com.postgres.postgre.repo.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 @RestController
 public class PersonController {
 
@@ -17,6 +18,35 @@ public class PersonController {
     @PostMapping("/addPerson")
     public void addPerson(@RequestBody Person person){
         repo.save(person);
+    }
+
+    @GetMapping("/getPerson/{id}")
+    public Optional<Person> getPerson(@PathVariable Long id) {
+        return repo.findById(id);
+    }
+
+    @GetMapping("/getAllPersons")
+    public List<Person> getAllPersons() {
+        return repo.findAll();
+    }
+
+
+
+    // Update a person
+    @PutMapping("/{id}")
+    public Person updatePerson(@PathVariable Long id, @RequestBody Person personDetails) {
+        Person person = repo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person not found with id " + id));
+
+        person.setName(personDetails.getName());
+
+        return repo.save(person);
+    }
+
+    // Delete a person
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        Person person = repo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person not found with id " + id));
+        repo.delete(person);
     }
 
 }
--- src/main/java/com/postgres/postgre/model/Person.java
+++ src/main/java/com/postgres/postgre/model/Person.java
@@ -1,15 +1,31 @@
 package com.postgres.postgre.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
-import jakarta.persistence.Id;
 
 @Data
 @Entity
 @Table(name = "person")
 public class Person {
     @Id  //for primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
